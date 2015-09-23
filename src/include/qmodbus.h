ea1bc47e66119a8bd3326d57c9b040863549b84b
KoynovStas <skojnov@yandex.ru>
2015-09-23 10:43:00 +0300

    add QModBus_TCP and QModBus_RTU class

--- src/include/qmodbus.h
+++ src/include/qmodbus.h
@@ -2,6 +2,8 @@
 #define QMODBUS_H
 
 #include <QObject>
+#include <QString>
+#include <QMutex>
 #include <QMetaType>
 
 
@@ -34,18 +36,84 @@ class QModBus : public QObject
          virtual ~QModBus();
 
 
+
     protected:
 
         explicit QModBus();
         QModBus(const QModBus& src);
         QModBus& operator=(const QModBus&);
 
+
+        virtual modbus_t* create_ctx() = 0;
+
+        modbus_t *mb_ctx;
+        QMutex    mb_ctx_mutex;
 };
 
 
 
 Q_DECLARE_METATYPE(QModBus::ModBusError)
 
+
+
+
+
+class QModBus_TCP : public QModBus
+{
+    Q_OBJECT
+
+
+    public:
+
+        explicit QModBus_TCP();
+
+
+        QString IP;
+        int     port;
+
+
+
+    protected:
+
+        //no copy constructor
+        QModBus_TCP(const QModBus_TCP& src);
+        QModBus_TCP& operator=(const QModBus_TCP&);
+
+
+        virtual modbus_t* create_ctx();
+};
+
+
+
+
+
+class QModBus_RTU : public QModBus
+{
+    Q_OBJECT
+
+
+    public:
+
+        explicit QModBus_RTU();
+
+
+        QString device;
+        int     baudrate;
+        char    parity;
+        int     data_bit;
+        int     stop_bit;
+
+
+
+    protected:
+
+        //no copy constructor
+        QModBus_RTU(const QModBus_RTU& src);
+        QModBus_RTU& operator=(const QModBus_RTU&);
+
+
+        virtual modbus_t* create_ctx();
+};
 
 
 
--- src/qmodbus.cpp
+++ src/qmodbus.cpp
@@ -18,3 +18,60 @@ QModBus::~QModBus()
 {
 
 }
+
+
+
+
+
+//---------------------- QModBus_TCP ----------------------
+
+
+
+
+
+QModBus_TCP::QModBus_TCP() :
+    QModBus(),
+
+    //public
+    IP("127.0.0.1"),
+    port(MODBUS_TCP_DEFAULT_PORT)
+{
+
+}
+
+
+
+modbus_t* QModBus_TCP::create_ctx()
+{
+    return modbus_new_tcp(IP.toStdString().c_str(), port);
+}
+
+
+
+
+
+//---------------------- QModBus_RTU ----------------------
+
+
+
+
+
+QModBus_RTU::QModBus_RTU() :
+    QModBus(),
+
+    //public
+    device("/dev/ttyUSB0"),
+    baudrate(115200),
+    parity('N'),
+    data_bit(8),
+    stop_bit(1)
+{
+
+}
+
+
+
+modbus_t* QModBus_RTU::create_ctx()
+{
+    return modbus_new_rtu(device.toStdString().c_str(), baudrate, parity, data_bit, stop_bit);
+}
